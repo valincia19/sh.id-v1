50504f910828d27d575a1c09ac6e18714833849e
Valincia User <valincia@user.com>
2026-03-12 17:12:28 +0700

    fix: use default import for config in discord.bot.js

--- backend/src/modules/discord/discord.bot.js
+++ backend/src/modules/discord/discord.bot.js
@@ -1,6 +1,6 @@
 import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
 import logger from "../../utils/logger.js";
-import { config } from "../../config/index.js";
+import config from "../../config/index.js";
 
 class DiscordBotService {
     constructor() {
